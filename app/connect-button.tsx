4dc92ae49b81235569336493280d1d0c004d7779
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-02-16 09:36:38 +0100

    add socket connection on button click

--- app/connect-button.tsx
+++ app/connect-button.tsx
@@ -1,16 +1,16 @@
 "use client";
+import { io } from "socket.io-client";
+
 type Props = {};
 
 export default function ConnectButton({}: Props) {
+  function connectToSocketServer() {
+    console.log("Connecting to the server...");
+    const connection = io(process.env.NEXT_PUBLIC_SOCKETS_URL);
+  }
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log("Connecting to server");
-        }}
-      >
-        Connect
-      </button>
+      <button onClick={connectToSocketServer}>Connect</button>
     </div>
   );
 }
